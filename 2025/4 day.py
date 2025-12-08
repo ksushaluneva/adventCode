e8d25488d61fbd77836c8ebf6940297c50b87c7f
ksushaluneva <99076279+ksushaluneva@users.noreply.github.com>
2025-12-08 20:10:54 +0200

    Update 4 day.py

--- 2025/4 day.py
+++ 2025/4 day.py
@@ -25,3 +25,35 @@ for i in range(1, l+1):
             if (arr[i-1][(j-1):(j+2)].count('@') + arr[i][(j-1):(j+2)].count('@') + arr[i+1][(j-1):(j+2)].count('@') -1) < 4:
                 c+=1
 print(c)
+
+# 4.2
+
+l = 139
+arr = []
+s = input()
+s = list("." + s + ".")
+ls = len(s) + 2
+arr.append([])
+for i in range(ls):
+    arr[0].append('.')
+arr.append(s)
+for j in range(l-1):
+    s = input()
+    s = list("." + s + ".")
+    arr.append(s)
+c = 0
+arr.append([])
+for i in range(ls):
+    arr[-1].append('.')
+# print(arr)
+cou = 1
+while cou != 0:
+    cou = 0
+    for i in range(1, l+1):
+        for j in range(1, ls-2):
+            if arr[i][j] == '@':
+                if (arr[i-1][(j-1):(j+2)].count('@') + arr[i][(j-1):(j+2)].count('@') + arr[i+1][(j-1):(j+2)].count('@') -1) < 4:
+                    c+=1
+                    arr[i][j] = '.'
+                    cou = 1
+print(c)
